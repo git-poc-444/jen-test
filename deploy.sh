9533b639e22ad180781e8c64a324174d8005904d
Naveena Kumari <naveena@letsmobility.com>
2020-07-25 09:49:17 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 sshpass -p Lets@1118 ssh ubuntu@$DEPLOY_SERVER <<EOF       
 echo "Project Folder"
 cd /var/www/test-angular-cicd
-git pull origin/master  
-rm -rf dist/pwa-example
-mkdir dist/pwa-example
-scp -r dist/ dist/pwa-example/
+sudo git pull origin/master  
+sudo rm -rf dist/pwa-example
+sudo mkdir dist/pwa-example
+sudo scp -r dist/ dist/pwa-example/
 sudo systemctl restart nginx
 exit      
 EOF
